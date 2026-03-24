7f1e169997576aea5decf4a3fe5d11d52b36a781
Murari0323 <murarimurari1808@gmail.com>
2026-03-24 09:29:19 +0530

    Add deployment config for Render (gunicorn, build.sh, render.yaml, .gitignore, dotenv)

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ Update MYSQL_PASSWORD and other settings to match your local MySQL setup.
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load .env file for local development
 
 
 class Config:
@@ -15,7 +18,7 @@ class Config:
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
     MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 's6z3ud7e0f')  # <-- Set your MySQL root password here
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')  # Set via environment variable
     MYSQL_DB = os.environ.get('MYSQL_DB', 'harvesthub')
     MYSQL_CURSORCLASS = 'DictCursor'
 
